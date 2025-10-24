f4ea43581070c4752f0cd709ecfaffc8b281f971
Muhanmmad Anas <muhammadanasflutter@gmail.com>
2025-10-24 10:18:06 +0500

    my work done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shoply/utils/theme_data.dart';
+import 'package:shoply/view/user_view/detail_screen.dart';
+import 'package:shoply/view/user_view/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+    return ScreenUtilInit(
+      designSize: Size(428, 923),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: ThemeMode.system,
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,3 +1,8 @@
 class AppImages {
-  static String logo = '';
+  static String profilePhoto = 'images/Ellipse 225.png';
+  static String book_1 = 'images/image 103.png';
+  static String book_2 = 'images/image 98.png';
+  static String book_3 = 'images/image 102.png';
+  static String book_4 = 'images/image 103 (1).png';
+  static String book_5 = 'images/image 106.png';
 }
